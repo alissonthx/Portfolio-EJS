9ce2eef62e2fee5adb9e30985f19fb2e6c1ffb70
alissonthx <alissonthx@hotmail.com>
2020-10-19 10:30:41 -0300

    Adicionado index.ejs para servir as paginas html e instalado nodemon

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const port = 3000;
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res, next) => {
-    res.send({
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res, next) {
+    res.render('index', {
         title:"My First Express Server",
         version: "0.0.0"
     });
