a06b28f32d9e966d34c945fe83503ac4a335e21e
alissonthx <alissonthx@hotmail.com>
2020-10-19 12:08:32 -0300

    Adicionado link para curriculo e conteudo.

--- app.js
+++ app.js
@@ -6,6 +6,25 @@ const path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.get('/curriculo', (req, res, next) => {
+    res.render('curriculo', {
+        title: 'Meu currículo',
+        name: 'Álisson Miquelace',
+        profession: 'Auxiliar de T.I.',
+        description: 'Auxiliar de tecnologia da informação com experiencia windows e linux',
+        experience: [{
+            company: 'Unifcv',
+            office: 'Auxiliar de T.I.',
+            description: 'Trabalho dando suporte aos usuarios docentes, colaboradores e alunos.'
+        }],
+        education: [{
+            instution: 'Feitep',
+            description: 'Engenharia elétrica (Trancado)'
+        }],
+        skills: ['Javascript', 'Node Js', 'HTML5', 'CSS']
+    });
+    });
+
 app.get('/', function(req, res, next) {
     res.render('index', {
         title:"My First Express Server",
